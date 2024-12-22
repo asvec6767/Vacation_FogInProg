099f952164f5ea6322d5a68d559e609b9711d751
asvec6767 <90004068+asvec6767@users.noreply.github.com>
2024-12-22 18:18:08 +0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
-#openai.api_key='token' #openai, no money
+#openai.api_key='gpt_key2' #openai, no money
 
 client = OpenAI(
-    api_key="token", #proxy api
+    api_key="gpt_key", #proxy api
     base_url="https://api.proxyapi.ru/openai/v1",
 )
 
@@ -12,7 +12,7 @@ def prompt(place:str):
         model="gpt-3.5-turbo-1106",
         messages=[{"role": "user", "content": query}],
         temperature=0.7,
-        max_tokens=150,
+        max_tokens=75,
         n=1,
         stop=None
     )
@@ -33,4 +33,18 @@ def prompt_distance_name(places):
     result = response.choices[0].message.content.__str__()
     return result
 
-#prompt("Тульский кремль")
+def prompt_check_poem(poem:str):
+    query="Ответь только 'да' или 'нет'. Связана ли эта фраза с новым годом, рождеством или зимой: '"+poem+"'"
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[{"role": "user", "content": query}],
+        temperature=0.7,
+        max_tokens=10,
+        n=1,
+        stop=None
+    )
+    result = response.choices[0].message.content.__str__()
+    if result == 'да' or result == 'Да' or result == 'ДА' or result == 'дА':
+        return True
+    else:
+        return False
